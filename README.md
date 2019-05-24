e56d86c4de3c177eccf428cde3fc9e15fa1222d7
Hugo BITARD <hugo.a.bitard@sogeti.com>
2019-05-24 09:43:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # redpi
 red team tools to install into a rpi
+
+## install
+
+```
+sudo setup.sh
+```
+
+## autossh
+
+In order to have a remote connexion to the raspberry:
+```bash
+ssh-keygen -t rsa -b 4096
+ssh-copy-id ~/.ssh/id_rsa.pub user@domain -> to copy keys to the remote server
+ssh user@domain -> to verify that we do not need password anymore to connect
+```
+
+Then create a service that will autossh to your domain at boot time:
+```bash
+sudo vim /etc/systemd/system/autossh.service
+
+[Unit]
+Description="une description du service"
+Wants=network-online.target ssh.target
+After=network-online.target ssh.target
+
+[Service]
+#Type=simple
+User=pi
+Environment="AUTOSSH_GATETIME 0"
+ExecStart=/usr/bin/autossh -N -R 2222:localhost:22 user@domain -p 22 -i ~/.ssh/id_rsa
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+```
+
+You need to reload the services list:
+
+```bash
+sudo systemctl daemon-reload
+sudo systemctl enable autossh.service
+sudo reboot
+```
+
+Then you could connect to your domain, then:
+```bash
+ssh pi@localhost -p 2222
+```
---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/usr/bash
+
+apt-get update
+apt-get -y upgrade
+apt-get install -y git
+apt-get install -y python-pip 
+apt-get install -y python3-pip 
+apt-get install -y libssl-dev libffi-dev python-dev build-essential
+apt-get install -y nbtscan 
+apt-get install -y python-scapy
+apt-get install -y wireshark-common 
+apt-get install -y tcpdump 
+apt-get install -y nmap  
+apt-get install -y python-bluez 
+apt-get install -y python-requests
+apt-get install -y python-urllib3
+apt-get install -y ppp 
+apt-get install -y xprobe2 
+apt-get install -y sg3-utils 
+apt-get install -y netdiscover 
+apt-get install -y macchanger 
+apt-get install -y unzip
+apt-get install -y vim
+apt-get install -y autossh
+apt-get install -y openssh-client
+apt-get install -y onesixtyone 
+apt-get install -y bridge-utils 
+apt-get install -y ettercap-text-only 
+apt-get install -y ike-scan 
+apt-ge	install -y proxychains
+apt-get install -y zsh
+
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O /tmp/install.sh
+chmod +x /tmp/install.sh
+./tmp/install.sh
+wget http://seclists.org/nmap-dev/2016/q2/att-201/clamav-exec.nse -O /usr/share/nmap/scripts/clamav-exec.nse
+
+pip install crackmapexec
+pip install python-nmap 
+pip install optparse-pretty 
+pip install netaddr 
+pip install prettytable 
+pip install ipaddress
+
+mkdir Tools
+cd Tools
+git clone https://github.com/SpiderLabs/Responder.git
+git clone https://github.com/portcullislabs/enum4linux.git
+git clone https://github.com/samratashok/nishang.git
+git clone https://github.com/PowerShellMafia/PowerSploit.git
+git clone https://github.com/EmpireProject/Empire.git
+cd Empire
+./setup/install.sh
+cd ..
+git clone https://github.com/AlessandroZ/LaZagne.git
+cd LaZagne
+pip install -r requirements.txt
+wget https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.2/lazagne.exe
+cd ..
+wget https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1
+apt-get install sqlite3 libsqlite3-dev
+cd  ..
+git clone https://github.com/klsecservices/rpivot.git
+cd rpivot
+wget https://github.com/klsecservices/rpivot/releases/download/v1.0/client.exe
+cd ..
+git clone https://github.com/sense-of-security/ADRecon.git
+git clone https://github.com/pentestmonkey/windows-privesc-check.git
+git clone https://github.com/darkoperator/dnsrecon.git
+git clone https://github.com/dafthack/MailSniper.git
+git clone https://github.com/SecureAuthCorp/impacket.git
+cd impacket
+pip install .
+cd ..
+
